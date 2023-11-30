c786f3c9340555aec55ed14f537232b16482894f
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-11-30 08:09:30 +0100

    feat(auth): add basic layout structure

--- apps/web/app/(app)/page.tsx
+++ apps/web/app/(app)/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { DemoSection } from '@/sections/demo-section';
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
