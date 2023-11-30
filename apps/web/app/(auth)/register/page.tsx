c306d9ac4b4f18c065146f5226747c82b2c81ef0
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-11-30 08:34:57 +0100

    feat(auth): add register page

--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -2,29 +2,46 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <>
       <Link
-        href="/register"
+        href="/login"
         className={cn(
           buttonVariants({ variant: 'ghost' }),
           'absolute right-4 top-4 md:right-8 md:top-8'
         )}
       >
-        Register
+        Login
       </Link>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Login to your account
+              Create an account
             </h1>
             <p className="text-sm text-muted-foreground">
-              Enter your email and password below to login
+              Enter your email below to create your account
             </p>
           </div>
           {/* <UserAuthForm /> */}
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
         </div>
       </div>
     </>
