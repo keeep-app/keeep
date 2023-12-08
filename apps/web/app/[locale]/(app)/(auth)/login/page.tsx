6dfb93786c01f82807ec30ffb312d71672fef1b4
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-08 09:37:33 +0100

    feat(i18n): add i18n to login and register page

--- apps/web/app/[locale]/(app)/(auth)/login/page.tsx
+++ apps/web/app/[locale]/(app)/(auth)/login/page.tsx
@@ -1,19 +1,19 @@
-'use client';
-
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { Button, buttonVariants } from '@/components/ui/button';
-import { useSupabase } from '@/lib/provider/supabase';
+import { buttonVariants } from '@/components/ui/button';
 import { UserAuthForm } from '@/components/user-auth-form';
-import { useRouter } from 'next/navigation';
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from 'next-intl';
+import { IntlMessages } from '@/lib/types/global';
+import pick from 'lodash/pick';
 
 export default function LoginPage() {
-  const { supabase } = useSupabase();
-  const router = useRouter();
+  const t = useTranslations('LoginPage');
+  const messages = useMessages() as IntlMessages;
 
-  if (!supabase) {
-    return null;
-  }
   return (
     <>
       <Link
@@ -23,19 +23,19 @@ export default function LoginPage() {
           'absolute right-4 top-4 md:right-8 md:top-8'
         )}
       >
-        Register
+        {t('registerButton')}
       </Link>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Login to your account
+              {t('title')}
             </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email and password below to login
-            </p>
+            <p className="text-sm text-muted-foreground">{t('description')}</p>
           </div>
-          <UserAuthForm type="login" className="mt-8" />
+          <NextIntlClientProvider messages={pick(messages, 'LoginForm')}>
+            <UserAuthForm type="login" className="mt-8" />
+          </NextIntlClientProvider>
         </div>
       </div>
     </>
--- apps/web/app/[locale]/(app)/(auth)/register/page.tsx
+++ apps/web/app/[locale]/(app)/(auth)/register/page.tsx
@@ -2,8 +2,18 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 import { UserAuthForm } from '@/components/user-auth-form';
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from 'next-intl';
+import { pick } from 'lodash';
+import { IntlMessages } from '@/lib/types/global';
 
 export default function RegisterPage() {
+  const t = useTranslations('RegisterPage');
+  const messages = useMessages() as IntlMessages;
+
   return (
     <>
       <Link
@@ -13,35 +23,38 @@ export default function RegisterPage() {
           'absolute right-4 top-4 md:right-8 md:top-8'
         )}
       >
-        Login
+        {t('loginButton')}
       </Link>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
+              {t('title')}
             </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
-            </p>
+            <p className="text-sm text-muted-foreground">{t('description')}</p>
           </div>
-          <UserAuthForm type="register" className="mt-8" />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking register, you agree to our{' '}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
+          <NextIntlClientProvider messages={pick(messages, 'RegisterForm')}>
+            <UserAuthForm type="register" className="mt-8" />
+          </NextIntlClientProvider>
+          <p className="text-center text-sm text-muted-foreground">
+            {t.rich('hint', {
+              terms: text => (
+                <Link
+                  href="/terms"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  {text}
+                </Link>
+              ),
+              privacy: text => (
+                <Link
+                  href="/privacy"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  {text}
+                </Link>
+              ),
+            })}
           </p>
         </div>
       </div>
