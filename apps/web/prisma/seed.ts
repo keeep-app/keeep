185e86c78a489b4ae96a55e69aa42cb46454def5
Konstantin <konstantin.muenster@mail.com>
2023-12-08 18:03:03 +0100

    feat(prisma): add attribute config; update seed

--- apps/web/prisma/seed.ts
+++ apps/web/prisma/seed.ts
@@ -1,27 +1,149 @@
-import { PrismaClient, AttributeType } from '@prisma/client';
+import { PrismaClient, Attribute } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const systemAttributes: {
-  label: string;
-  type: AttributeType;
-  icon?: string;
-}[] = [
+const attributeConfig: Omit<
+  Attribute,
+  'updatedAt' | 'createdAt' | 'id' | 'organizationId' | 'icon' | 'description'
+>[] = [
   {
-    label: 'Profile Picture',
-    type: 'TEXT',
-  },
-  {
+    system: true,
+    required: false,
     label: 'First Name',
     type: 'TEXT',
+    config: {},
   },
   {
+    system: true,
+    required: false,
     label: 'Last Name',
     type: 'TEXT',
+    config: {},
   },
   {
+    system: true,
+    required: false,
     label: 'Email Address',
     type: 'EMAIL',
+    config: {},
+  },
+  {
+    system: false,
+    required: false,
+    label: 'Status',
+    type: 'SELECT',
+    config: {
+      options: [
+        {
+          label: 'New',
+          value: 'new',
+          color: 'gray',
+        },
+        {
+          label: 'Contacted',
+          value: 'contacted',
+          color: 'blue',
+        },
+        {
+          label: 'Qualified',
+          value: 'qualified',
+          color: 'green',
+        },
+        {
+          label: 'Proposal Sent',
+          value: 'proposal',
+          color: 'yellow',
+        },
+        {
+          label: 'Negotiation',
+          value: 'negotiation',
+          color: 'orange',
+        },
+        {
+          label: 'Won',
+          value: 'won',
+          color: 'green',
+        },
+        {
+          label: 'Lost',
+          value: 'lost',
+          color: 'red',
+        },
+      ],
+    },
+  },
+];
+
+const contacts = [
+  {
+    externalId: '1',
+    status: 'new',
+    email: 'john.doe@example.com',
+    firstName: 'John',
+    lastName: 'Doe',
+  },
+  {
+    externalId: '2',
+    status: 'contacted',
+    email: 'jane.doe@example.com',
+    firstName: 'Jane',
+    lastName: 'Doe',
+  },
+  {
+    externalId: '3',
+    status: 'qualified',
+    email: 'bob.smith@example.com',
+    firstName: 'Bob',
+    lastName: 'Smith',
+  },
+  {
+    externalId: '4',
+    status: 'proposal',
+    email: 'alice.jones@example.com',
+    firstName: 'Alice',
+    lastName: 'Jones',
+  },
+  {
+    externalId: '5',
+    status: 'negotiation',
+    email: 'charlie.brown@example.com',
+    firstName: 'Charlie',
+    lastName: 'Brown',
+  },
+  {
+    externalId: '6',
+    status: 'won',
+    email: 'david.johnson@example.com',
+    firstName: 'David',
+    lastName: 'Johnson',
+  },
+  {
+    externalId: '7',
+    status: 'lost',
+    email: 'emily.williams@example.com',
+    firstName: 'Emily',
+    lastName: 'Williams',
+  },
+  {
+    externalId: '8',
+    status: 'new',
+    email: 'frank.thomas@example.com',
+    firstName: 'Frank',
+    lastName: 'Thomas',
+  },
+  {
+    externalId: '9',
+    status: 'contacted',
+    email: 'grace.martin@example.com',
+    firstName: 'Grace',
+    lastName: 'Martin',
+  },
+  {
+    externalId: '10',
+    status: 'qualified',
+    email: 'harry.edwards@example.com',
+    firstName: 'Harry',
+    lastName: 'Edwards',
   },
 ];
 
@@ -32,15 +154,18 @@ async function main() {
       name: 'keeep',
       attributes: {
         createMany: {
-          data: systemAttributes.map(a => ({ ...a, system: true })),
+          data: attributeConfig.map(a => ({
+            ...a,
+            config: a.config ?? {},
+          })),
         },
       },
       users: {
         create: [
           {
-            email: 'user@example.com',
-            firstName: 'Test',
-            lastName: 'User',
+            email: 'dummy@keeep.app',
+            firstName: 'Bobby',
+            lastName: 'Crown',
           },
         ],
       },
@@ -56,6 +181,7 @@ async function main() {
   const attributeFirstName = attributes.find(a => a.label === 'First Name');
   const attributeLastName = attributes.find(a => a.label === 'Last Name');
   const attributeEmail = attributes.find(a => a.label === 'Email Address');
+  const attributeStatus = attributes.find(a => a.label === 'Status');
 
   await prisma.list.create({
     data: {
@@ -63,28 +189,24 @@ async function main() {
       slug: 'my-list',
       name: 'My List',
       contacts: {
-        create: [
-          {
-            externalId: '1',
-            organizationId: org.id,
-            attributes: {
-              ...(attributeFirstName && { [attributeFirstName.id]: 'John' }),
-              ...(attributeLastName && { [attributeLastName.id]: 'Doe' }),
-              ...(attributeEmail && {
-                [attributeEmail.id]: 'john.doe@example.com',
-              }),
-            },
-            companies: {
-              create: [
-                {
-                  slug: 'apple',
-                  name: 'Apple',
-                  organizationId: org.id,
-                },
-              ],
-            },
+        create: contacts.map(contact => ({
+          externalId: contact.externalId,
+          organizationId: org.id,
+          attributes: {
+            ...(attributeFirstName && {
+              [attributeFirstName.id]: contact.firstName,
+            }),
+            ...(attributeLastName && {
+              [attributeLastName.id]: contact.lastName,
+            }),
+            ...(attributeEmail && {
+              [attributeEmail.id]: contact.email,
+            }),
+            ...(attributeStatus && {
+              [attributeStatus.id]: contact.status,
+            }),
           },
-        ],
+        })),
       },
     },
   });
