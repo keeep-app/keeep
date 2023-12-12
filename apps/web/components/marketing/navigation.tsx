07d3a518f395f6cd00123d9eefedf7d4970a88de
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-12 15:39:11 +0100

    feat(landing-page): update navigation

--- apps/web/components/marketing/navigation.tsx
+++ apps/web/components/marketing/navigation.tsx
@@ -1,39 +1,45 @@
 import { MountainIcon } from 'lucide-react';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export const Navigation: React.FC = () => {
   return (
     <header className="flex h-14 items-center px-4 lg:px-6">
-      <Link className="flex items-center justify-center" href="#">
-        <MountainIcon className="mr-2 h-6 w-6" />
-        <span>Keeep</span>
-      </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Features
+      <div className="flex items-center space-x-10">
+        <Link className="flex items-center justify-center" href="#">
+          <MountainIcon className="mr-2 h-6 w-6" />
+          <span className="text-xl font-bold">Keeep</span>
         </Link>
-        <Link
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Pricing
-        </Link>
-        <Link
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          About
-        </Link>
-        <Link
-          className="text-sm font-medium underline-offset-4 hover:underline"
-          href="#"
-        >
-          Contact
-        </Link>
-      </nav>
+        <nav className="flex gap-4 sm:gap-6">
+          <Link
+            className="text-sm font-medium underline-offset-4 hover:underline"
+            href="#"
+          >
+            Features
+          </Link>
+          <Link
+            className="text-sm font-medium underline-offset-4 hover:underline"
+            href="#"
+          >
+            Pricing
+          </Link>
+          <Link
+            className="text-sm font-medium underline-offset-4 hover:underline"
+            href="#"
+          >
+            About
+          </Link>
+          <Link
+            className="text-sm font-medium underline-offset-4 hover:underline"
+            href="#"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
+      <Button asChild className="ml-auto" size="sm">
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
     </header>
   );
 };
