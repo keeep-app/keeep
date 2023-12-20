656a478691ae6cbb596475ef078bb8d76ec04e0c
Konstantin <konstantin.muenster@mail.com>
2023-12-20 20:43:56 +0100

    format: codebase

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,15 +1,15 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
   overrides: [
     {
-      extends: ["plugin:@typescript-eslint/disable-type-checked"],
-      files: ["./**/*.js", "./**/*.jsx", "./**/*.cjs"],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.js', './**/*.jsx', './**/*.cjs'],
     },
   ],
 };
