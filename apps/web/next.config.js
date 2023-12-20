5df68e981b52eee257588784644a0cbb29314888
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-20 21:43:46 +0100

    fix: use default vercel system public env

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,10 @@ const withNextIntl = require('next-intl/plugin')();
 /** @type {import('next').NextConfig} */
 const config = {
   transpilePackages: ['@repo/ui'],
+  env: {
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
+  },
 };
 
 module.exports = withNextIntl(config);
