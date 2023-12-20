34e04fc7d0d03eb3dc81d78146bcb1c67ee7ab6a
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-20 21:44:04 +0100

    feat(register): add loading and clear form on success

--- apps/web/components/user-auth-form.tsx
+++ apps/web/components/user-auth-form.tsx
@@ -20,6 +20,7 @@ import { cn } from '@/lib/utils';
 import { useToast } from './ui/use-toast';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface UserAuthFormProps {
   type: 'login' | 'register';
@@ -44,8 +45,11 @@ export const UserAuthForm: React.FC<UserAuthFormProps> = ({
     resolver: zodResolver(formSchema),
   });
 
+  const [loading, setLoading] = useState(false);
+
   async function onSubmit(values: z.infer<typeof formSchema>) {
     if (!supabase) throw new Error('Supabase client not found');
+    setLoading(true);
     if (type === 'login') {
       const { data, error } = await supabase!.auth.signInWithPassword({
         email: values.email,
@@ -53,6 +57,7 @@ export const UserAuthForm: React.FC<UserAuthFormProps> = ({
       });
       console.log('user', data);
       if (error) {
+        setLoading(false);
         toast({
           title: 'Login failed',
           description: error.message,
@@ -75,18 +80,22 @@ export const UserAuthForm: React.FC<UserAuthFormProps> = ({
 
       if (error) {
         if (error.message === 'User already registered') {
+          setLoading(false);
           toast({
             title: 'Registration failed',
             description:
               'This email is already registered. Please go to the login page.',
           });
         } else {
+          setLoading(false);
           toast({
             title: 'Registration failed',
             description: error.message,
           });
         }
       } else {
+        form.reset();
+        setLoading(false);
         toast({
           title: 'Registration successful',
           description: 'Please check your inbox for the confirmation link.',
@@ -135,8 +144,47 @@ export const UserAuthForm: React.FC<UserAuthFormProps> = ({
             )}
           />
         </div>
-        <Button type="submit" className="mt-6 w-full">
-          {type === 'login' ? 'Login' : 'Register'}
+        <Button
+          disabled={loading}
+          type="submit"
+          className="relative mt-6 w-full"
+        >
+          <span
+            className={cn({
+              'opacity-0': loading,
+              'opacity-100': !loading,
+            })}
+          >
+            {type === 'login' ? 'Login' : 'Register'}
+          </span>
+          <span
+            className={cn('absolute inset-0 flex items-center justify-center', {
+              'opacity-100': loading,
+              'opacity-0': !loading,
+            })}
+          >
+            <svg
+              className="h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth={4}
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </span>
         </Button>
       </form>
     </Form>
