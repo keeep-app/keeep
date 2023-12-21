3b6643cf146e46e6f26621b76b912bd32efe833e
Konstantin <konstantin.muenster@mail.com>
2023-12-21 19:07:01 +0100

    fix(route-handler): add authorization check

--- apps/web/app/api/organization/lists/route.ts
+++ apps/web/app/api/organization/lists/route.ts
@@ -1,10 +1,19 @@
 import { prisma } from '@/lib/server/prisma';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse as Response } from 'next/server';
 
 export async function GET(req: NextRequest) {
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+
+  const userResponse = await supabase.auth.getUser();
+  if (!userResponse.data.user) {
+    return Response.json({ error: 'Not authenticated' }, { status: 401 });
+  }
+
   const searchParams = req.nextUrl.searchParams;
   const slug = searchParams.get('organization-slug');
-
   if (!slug) {
     return Response.json(
       { error: 'No organization slug specified' },
@@ -12,11 +21,11 @@ export async function GET(req: NextRequest) {
     );
   }
 
+  const supabaseId = userResponse.data.user.id;
   const organization = await prisma.organization.findUnique({
-    where: { slug },
+    where: { slug, members: { some: { supabaseId } } },
     include: { lists: true },
   });
-
   if (!organization) {
     return Response.json({ error: 'No organization found' }, { status: 404 });
   }
--- apps/web/lib/swr.ts
+++ apps/web/lib/swr.ts
@@ -2,7 +2,12 @@ import useNativeSWR from 'swr';
 
 export function useSWR<T>(url: string): ReturnType<typeof useNativeSWR<T>> {
   return useNativeSWR<T>(url, async () => {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        credentials: 'include',
+      },
+    });
     return (await response.json()).data;
   });
 }
