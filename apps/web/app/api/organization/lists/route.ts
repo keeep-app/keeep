f666b78c8d03970294cc7f7ce805c2893bf43eff
Konstantin <konstantin.muenster@mail.com>
2023-12-21 19:09:46 +0100

    refactor(route-handler): use helper function

--- apps/web/app/api/organization/lists/route.ts
+++ apps/web/app/api/organization/lists/route.ts
@@ -1,19 +1,15 @@
-import { prisma } from '@/lib/server/prisma';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
 import { NextRequest, NextResponse as Response } from 'next/server';
 
-export async function GET(req: NextRequest) {
-  const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+import { prisma } from '@/lib/server/prisma';
+import { getSupabaseRouteHandlerClient } from '@/lib/server/supabase';
 
-  const userResponse = await supabase.auth.getUser();
-  if (!userResponse.data.user) {
+export async function GET(req: NextRequest) {
+  const { user } = await getSupabaseRouteHandlerClient();
+  if (!user) {
     return Response.json({ error: 'Not authenticated' }, { status: 401 });
   }
 
-  const searchParams = req.nextUrl.searchParams;
-  const slug = searchParams.get('organization-slug');
+  const slug = req.nextUrl.searchParams.get('organization-slug');
   if (!slug) {
     return Response.json(
       { error: 'No organization slug specified' },
@@ -21,11 +17,11 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  const supabaseId = userResponse.data.user.id;
   const organization = await prisma.organization.findUnique({
-    where: { slug, members: { some: { supabaseId } } },
+    where: { slug, members: { some: { supabaseId: user.id } } },
     include: { lists: true },
   });
+
   if (!organization) {
     return Response.json({ error: 'No organization found' }, { status: 404 });
   }
