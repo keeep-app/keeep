098bbd92e5453494c0d4d10ff7457c28bded9863
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-21 10:41:33 +0100

    chore: fix gitignore

---
+++ apps/web/lib/supabase/getServerClient.ts
@@ -0,0 +1,27 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+
+export const getSupabaseServerClient = (): SupabaseClient => {
+  const cookieStore = cookies();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options });
+        },
+      },
+    }
+  );
+
+  return supabase;
+};
