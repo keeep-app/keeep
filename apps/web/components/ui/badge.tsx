f7d7e3a7ce92e1d545c84ff678558651376d3e69
Konstantin <konstantin.muenster@mail.com>
2023-12-22 09:54:33 +0100

    fix(style): add tabular variant

--- apps/web/components/ui/badge.tsx
+++ apps/web/components/ui/badge.tsx
@@ -34,9 +34,12 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, variant, tabular, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div
+      className={cn(badgeVariants({ variant, tabular }), className)}
+      {...props}
+    />
   );
 }
 
