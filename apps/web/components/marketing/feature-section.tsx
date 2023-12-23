ce7f9da855191ea13d8688ca4ab8f51a00fa56c8
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-23 18:06:01 +0100

    feat: add additional entries to feature section

--- apps/web/components/marketing/feature-section.tsx
+++ apps/web/components/marketing/feature-section.tsx
@@ -3,12 +3,19 @@ import { useTranslations } from 'next-intl';
 
 export const FeatureSection: React.FC = () => {
   const t = useTranslations('FeatureSection');
-  const keys = ['import', 'contacts', 'customization', 'automation'] as const;
+  const keys = [
+    'import',
+    'contacts',
+    'customization',
+    'automation',
+    'notes',
+    'mobile',
+  ] as const;
   return (
     <section className="w-full">
       <div className="container px-4 md:px-6">
         <h2 className="mb-12 text-center text-3xl font-bold">{t('title')}</h2>
-        <div className="grid items-start gap-6 md:grid-cols-3">
+        <div className="grid items-start gap-6 md:grid-cols-3 md:grid-rows-2">
           {keys.map(key => (
             <Card key={key} className="h-full">
               <CardHeader>
