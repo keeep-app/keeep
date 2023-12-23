18051df49bf68f7a53a73808ea986f53e5ecf497
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2023-12-23 17:00:02 +0100

    chore: remove unused config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,6 @@ const withNextIntl = require('next-intl/plugin')();
 /** @type {import('next').NextConfig} */
 const config = {
   transpilePackages: ['@repo/ui'],
-  env: {
-    // eslint-disable-next-line turbo/no-undeclared-env-vars
-    NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
-  },
 };
 
 module.exports = withNextIntl(config);
