905b0e49ffac9f59dcbebc6981142126f84ec2c8
Marvin Splitt <marvin.splitt.extern@starfinanz.de>
2024-01-15 10:11:02 +0100

    feat(onboarding): check org on onboarding page

--- apps/web/app/[locale]/(app)/dashboard/onboarding/page.tsx
+++ apps/web/app/[locale]/(app)/dashboard/onboarding/page.tsx
@@ -1,9 +1,23 @@
 import { OnboardingDialog } from '@/components/onboarding/onboarding-dialog';
+import { prisma } from '@/lib/server/prisma';
+import { getSupabaseServerComponentClient } from '@/lib/server/supabase';
 import { pick } from 'lodash';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { redirect } from 'next/navigation';
 
-export default function OnboardingPage() {
-  const messages = useMessages() as IntlMessages;
+export default async function OnboardingPage() {
+  const { user } = await getSupabaseServerComponentClient();
+  if (!user) return redirect('/login');
+
+  const result = await prisma.organization.findFirst({
+    where: { members: { some: { id: user.id } } },
+  });
+
+  if (result) return redirect(`/dashboard/${result.slug}`);
+
+  const messages = (await getMessages()) as IntlMessages;
+
   return (
     <NextIntlClientProvider messages={pick(messages, 'OnboardingDialog')}>
       <OnboardingDialog />
